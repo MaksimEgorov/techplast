37c5b51447d2e8002e8000d999e1abf3937b2501
MaksimEgorov <egorov.gps@gmail.com>
2019-03-14 07:44:58 +0000

    JS integration for products block

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,50 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* window.onload = function () {
+    document.getElementById('lenta').onmouseover=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion2.png")';
+    }
+    document.getElementById('lenta').onmouseout=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion.png")';
+    }
+
+    document.getElementById('geomembrana').onmouseover=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion3.png")';
+    }
+    document.getElementById('geomembrana').onmouseout=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion.png")';
+    }
+
+    document.getElementById('georeshetka').onmouseover=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion4.png")';
+    }
+    document.getElementById('georeshetka').onmouseout=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion.png")';
+    }
+
+    document.getElementById('reotekstil').onmouseover=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion5.png")';
+    }
+    document.getElementById('reotekstil').onmouseout=function() {
+        document.getElementById('products-images').style.cssText='background: url("../images/Gibion.png")';
+    }
+} */
